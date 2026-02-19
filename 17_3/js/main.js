d89668c5402a5dd5b5b3ae6c31babe83a4edc5e2
TSv100 <tatbook33@gmail.com>
2026-02-19 22:15:32 +0300

    Add files via upload

---
+++ 17_3/js/main.js
@@ -0,0 +1,24 @@
+let username = prompt("Введите имя пользователя:");
+let password = prompt("Введите пароль:");
+
+if ((username === "admin" || username === "user") && password === "123456") {
+    alert("Доступ разрешён");
+} else {
+    alert("Доступ запрещён");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
